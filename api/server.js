baebb6e029ba164441e597fae9f16080ebc528d7
Penny Tague <pmtague9@gmail.com>
2019-09-04 10:51:18 -0700

    Started setting up supplier router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = require('../suppliers/suppliers-router.js');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
 	res.status(200).json({ message: 'got it' });
---
+++ suppliers/suppliers-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get('/', req, res) => {
+	res.send('get to /suppliers/')
+}
+
+module.exports = router
